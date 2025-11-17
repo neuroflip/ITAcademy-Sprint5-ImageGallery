f9e36f3722fcf331074af2a8d1a74afcb1391ff3
neuroflip <jaanguita@gmail.com>
2025-11-17 10:35:37 +0100

    Added semanthic tags in App (header, footer and main). Aria-label for icon image

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,18 @@ import { data } from './data/images';
 
 function App() {
   return (<>
-    <div className="w-full px-4 flex justify-center items-center mt-5 mb-5 gap-2">
-      <img className="w-10 app__logoTitle" alt="Image Gallery icon" />
+    <header className="w-full px-4 flex justify-center items-center mt-10 mb-10 gap-2">
+      <img className="w-10 app__logoTitle" alt="Image Gallery icon" aria-label="Image icon for the gallery" />
       <h1 className="bold">Image Gallery</h1>
-    </div>
-    <Gallery images={ data }></Gallery>
+    </header>
+    
+    <main>
+      <Gallery images={ data }></Gallery>
+    </main>
+
+    <footer className="flex justify-center items-center m-10">
+      ITAcademy - Sprint 5
+    </footer>
   </>);
 }
 
