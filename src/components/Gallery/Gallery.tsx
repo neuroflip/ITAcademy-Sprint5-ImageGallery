2b332028df61818e2e1efba89220b1bab218ce17
n <n@ns-Mac-mini.local>
2025-11-18 19:51:12 +0100

    refactor: Added a custom hook to encapsulate the Gallery logic

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -4,21 +4,11 @@ import type { GalleryProps } from './Gallery.d';
 import type { ImagesData } from '@/data/images.d';
 
 import ImageItem from '../ImageItem/ImageItem';
+import useGallery from './hooks/useGallery';
 
 const Gallery = ({ images }: GalleryProps) => {
     const [ imagesData, setImagesData ] = React.useState<Array<ImagesData>>(images);
-    const onDelete = (id: string) => {
-        const newImagesData = [...imagesData];
-        const image = newImagesData.find((image) => image.id === id);
-        if (image) {
-            const index = newImagesData.indexOf(image);
-            
-            if (index !== -1) {
-                newImagesData.splice(index, 1);
-            }
-            setImagesData(newImagesData);
-        }
-    }
+    const onDelete = useGallery(imagesData, setImagesData);
 
     return <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-3">
             { imagesData.map((imageData, index) => (
