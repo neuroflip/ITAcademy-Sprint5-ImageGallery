f9cf1ae9aa0f3ae3bf2680a14eac940051ab1154
n <n@ns-Mac-mini.local>
2025-11-18 19:37:04 +0100

    fix: removed lint errors from last commit

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,6 +1,5 @@
 import { vi, describe, it, expect } from 'vitest';
 import { render } from '@testing-library/react'
-import { Gallery } from '../components/Gallery/Gallery';
 import App from '../App';
 
 vi.mock('../components/Gallery/Gallery', () => ({
