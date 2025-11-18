245ae9e504015c21d652c1baef8bfcae427ac084
n <n@ns-Mac-mini.local>
2025-11-18 19:33:40 +0100

    test: Added a Gallery mock in app test to avoid errors in CI

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,6 +1,11 @@
-import { describe, it, expect } from 'vitest';
+import { vi, describe, it, expect } from 'vitest';
 import { render } from '@testing-library/react'
+import { Gallery } from '../components/Gallery/Gallery';
 import App from '../App';
+
+vi.mock('../components/Gallery/Gallery', () => ({
+  default: vi.fn(()=>`<div></div>`)
+}))
 
 describe('App', () => {
   it('renders app component', () => {
